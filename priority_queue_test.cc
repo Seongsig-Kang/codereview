82a166aaae1cb85c2b95d801a1001ed2e37d4681
Seongsig-Kang <51217153+Seongsig-Kang@users.noreply.github.com>
2019-06-12 15:12:37 +0900

    Update priority_queue_test.cc

--- priority_queue_test.cc
+++ priority_queue_test.cc
@@ -1,52 +1,40 @@
 #include "priority_queue.h"
 #include <gtest/gtest.h>
 
-// TEST(testCaseName, testName){
-//   ... test body ...
-// }
-
 
 TEST(PriorityQueueTest,Trivial){
-	templatePriorityQueue<int> expTarget;
+	TemplatePriorityQueue expTarget;
 
 	//Test 1 - Initalize
-	ASSERT_TRUE(expTarget.empty()); // empty test
+	ASSERT_TRUE(expTarget.Empty()); // empty test
 
 	//Test 2 - Push Datas and Top size
-	expTarget.push(1);
-	expTarget.push(4);
-	expTarget.push(2);
-	expTarget.push(3);
-	expTarget.push(8);
-	expTarget.push(5);
+	expTarget.Push(1);
+	expTarget.Push(4);
+	expTarget.Push(2);
+	expTarget.Push(3);
+	expTarget.Push(8);
+	expTarget.Push(5);
 	
-	ASSERT_EQ(expTarget.size(),6); // Pushed 6 elements
-	ASSERT_EQ(expTarget.top(),8);  // The highest pushed value is 8
-	ASSERT_FALSE(expTarget.empty()); // it must not empty
+	ASSERT_EQ(expTarget.Size(),6); // Pushed 6 elements
+	ASSERT_EQ(expTarget.Top(),8);  // The highest pushed value is 8
+	ASSERT_FALSE(expTarget.Empty()); // it must not empty
 
 	//Test 3 - pop test
 
-	expTarget.pop();
-	expTarget.pop();
-	expTarget.pop();
+	expTarget.Pop();
+	expTarget.Pop();
+	expTarget.Pop();
 	
-	ASSERT_EQ(expTarget.top(),3);
-	ASSERT_EQ(expTarget.size(),3);
-	ASSERT_FALSE(expTarget.empty());
+	ASSERT_EQ(expTarget.Top(),3);
+	ASSERT_EQ(expTarget.Size(),3);
+	ASSERT_FALSE(expTarget.Empty());
 
-	expTarget.pop();
-	expTarget.pop();
-	expTarget.pop();
+	expTarget.Pop();
+	expTarget.Pop();
+	expTarget.Pop();
 	
-	ASSERT_TRUE(expTarget.empty());
-	
-	/* Fail Example - If you activate this code, Test will be fail *
-
-	expTarget.pop();
-	
-	EXPECT_TRUE(expTarget.empty()); // non-Fatal Failure - not terminate this test case when result of empty method is false.
-	ASSERT_EQ(expTarget.size(),0);
-        /**/
+	ASSERT_TRUE(expTarget.Empty());
 	
 }
 
