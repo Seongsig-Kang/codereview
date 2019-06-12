d5bc195b337ff98a6c80684adffd2dd8aa9bc9bf
Seongsig-Kang <51217153+Seongsig-Kang@users.noreply.github.com>
2019-06-12 15:13:48 +0900

    Update main.cc

--- main.cc
+++ main.cc
@@ -4,59 +4,57 @@
 
 using namespace std;
 
-template <typename T>
-void pushQueue(templatePriorityQueue<T>* Que, int data){
+void pushQueue(TemplatePriorityQueue* Que, int data) {
 	cout << "[+] Integer "<< data <<" pushed in the Priority Queue." << endl;
-	Que->push(data);
+	Que->Push(data);
 }
 
-template<typename T>
-void popQueue(templatePriorityQueue<T>* Que){
-	cout << "[+] Integer "<< Que->top() << " will be poped from the Priority Queue." << endl;
-	Que->pop();
+void popQueue(TemplatePriorityQueue* Que) {
+	cout << "[+] Integer "<< Que->Top() << " will be poped from the Priority Queue." << endl;
+	Que->Pop();
 }
 
-int main(int argc, char* argv[]){
-	templatePriorityQueue<int> priQueue;
+int main(int argc, char* argv[]) {
+	TemplatePriorityQueue priQueue;
 	int customNumber = 0;
 
 	//empty check
-	if(priQueue.empty()){
+	if(priQueue.Empty()){
 		cout << "[!] Priority Queue is empty - initalized" << endl;
 	}
 
 	// push
-	pushQueue(&priQueue,23);
-	pushQueue(&priQueue,598);
-	pushQueue(&priQueue,302);
+	pushQueue(&priQueue, 23);
+	pushQueue(&priQueue, 598);
+	pushQueue(&priQueue, 302);
 
 	if( argc > 1 ){
 		customNumber = atoi(argv[1]);
 		if( customNumber > -500 && customNumber < 2000 ){
-			pushQueue(&priQueue,customNumber);
+			pushQueue(&priQueue, customNumber);
 		}
 		else{
-			pushQueue(&priQueue,-1);
+			pushQueue(&priQueue, -1);
 		}
 	}
 	else{
-		pushQueue(&priQueue,-1);
+		pushQueue(&priQueue, -1);
 	}
 
 	//empty & size
-	if(priQueue.empty()){
+	if(priQueue.Empty()) {
 		cout << "[-] Priority Queue is empty. - Push method did not work!" << endl;
 		return -1;
 	}
 	else{
-		cout << "[+] Priority Queue has data. - Queue Size : " << priQueue.size() <<endl;
+		cout << "[+] Priority Queue has data. - Queue Size : " << priQueue.Size() <<endl;
 	}
 
 	// pop twice
 	popQueue(&priQueue);
 	popQueue(&priQueue);
 
-	cout << "[+] Now, Top element of Priority Queue is " << priQueue.top() << endl;
+	cout << "[+] Now, Top element of Priority Queue is " << priQueue.Top() << endl;
 	
 	return 0;
 }
